48cbd84b37afd5a255192eee7222ecedecc570bc
MarcosBL <marcosbl@gmail.com>
2013-09-01 01:50:58 +0200

    Tweaks to Install Development Tools

--- recipes/software.rb
+++ recipes/software.rb
@@ -20,7 +20,7 @@ namespace :soft do
         desc "Install Development Tools"
         task :install_dev do
 			sudo "apt-get install build-essential git-core git-svn openssl libssl-dev -y"
-			sudo "grep -q \"alias gitcommit='git commit -am'\" /root/.bashrc || echo \"alias gitcommit='git commit -am'\" >> /root/.bashrc"
+			sudo "grep -q \"alias gitcommit='git add *;git commit -am'\" /root/.bashrc || echo \"alias gitcommit='git add *;git commit -am'\" >> /root/.bashrc"
 			sudo "grep -q \"alias gitpush='git push origin master'\" /root/.bashrc || echo \"alias gitpush='git push origin master'\" >> /root/.bashrc"
         end
 
