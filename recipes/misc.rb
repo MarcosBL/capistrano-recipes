f7f73ee52d955ee333a283eb633d8b4226fcd58f
MarcosBL <marcosbl@gmail.com>
2013-09-01 01:30:30 +0200

    Reorder, leaving soft related functions together

--- recipes/misc.rb
+++ recipes/misc.rb
@@ -1,28 +1,5 @@
 Capistrano::Configuration.instance(true).load do
 namespace :misc do
-        desc "Remove problematic packages"
-        task :remove do
-                sudo "apt-get remove grub-pc"
-                sudo "apt-get -y autoremove"
-        end
-        desc "Update apt-get sources"
-        task :update do
-          sudo "apt-get update"
-        end
 
-        desc "Upgrade packages"
-        task :upgrade do
-          sudo "apt-get -y upgrade"
-        end
-
-        desc "Install Development Tools"
-        task :install_dev do
-          sudo "apt-get install build-essential -y"
-        end
-
-        desc "Install Git"
-        task :install_git do
-          sudo "apt-get install git-core git-svn -y"
-        end
 end
 end
